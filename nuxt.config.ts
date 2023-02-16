7c31b9aabd72d0c6a98393b20fcd103c7769a285
Nemanja Dragun <39500267+ndragun92@users.noreply.github.com>
2023-02-16 22:49:26 +0100

    Initial commit

---
+++ nuxt.config.ts
@@ -0,0 +1,42 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+import eslintPlugin from "vite-plugin-eslint";
+
+export default defineNuxtConfig({
+  nitro: {
+    compressPublicAssets: true,
+    routeRules: {
+      // "/_nuxt/**": { headers: { "cache-control": "max-age=31536000" } }, // Set generated files cache to 1 year
+    },
+  },
+  runtimeConfig: {
+    public: {
+      baseURL: "",
+    },
+  },
+  vite: {
+    plugins: [eslintPlugin()],
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `@import "@/assets/css/utils/global.scss";`,
+        },
+      },
+    },
+  },
+  css: ["~/assets/css/main.scss"],
+  modules: [
+    // https://pinia.vuejs.org/ssr/nuxt.html
+    "@pinia/nuxt",
+    // https://tailwindcss.nuxt.dev/
+    "@nuxtjs/tailwindcss",
+    // https://v1.image.nuxtjs.org/get-started/
+    "@nuxt/image-edge",
+    // https://github.com/nuxt-modules/icon
+    // https://icones.js.org/collection/all?s=github
+    "nuxt-icon",
+    // https://vueuse.org/guide/#nuxt
+    "@vueuse/nuxt",
+    // https://github.com/MorevM/vue-transitions
+    "@morev/vue-transitions/nuxt",
+  ],
+});
