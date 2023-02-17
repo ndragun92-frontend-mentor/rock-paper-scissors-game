a83b7e0de85e7c496cb4b30b85e33453e5cb3d84
ndragun92 <nemanjadragun92@gmail.com>
2023-02-17 17:16:14 +0100

    feat: add footer and copyrights

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,25 @@
 <template>
   <slot />
+  <footer
+    class="text-primary-light text-sm text-center md:text-left opacity-75 hover:opacity-100"
+  >
+    <div class="p-3">
+      Challenge by
+      <a
+        class="text-blue-500 hover:underline"
+        href="https://www.frontendmentor.io?ref=challenge"
+        target="_blank"
+        >Frontend Mentor</a
+      >
+      & Coded by
+      <a
+        class="text-blue-500 hover:underline"
+        href="https://ndragun92.github.io/portfolio/"
+        target="_blank"
+        >Nemanja Dragun</a
+      >
+    </div>
+  </footer>
 </template>
 
 <script lang="ts">
